e1ba837f6f98773deed5548475e326b01f759661
Vaibhav Pawar <pawar1vaibhav6@gmail.com>
2026-01-01 10:06:14 +0530

    Added score counter and Another Round

--- main.py
+++ main.py
@@ -23,3 +23,36 @@ def rps(p1, p2):
             return "You Won!"
         elif p2=="Scissors":
             return "You Lost!"
+        
+def main():
+    sys_score = 0
+    y_score = 0
+    rps_list = ["Rock", "Paper", "Scissors"]
+    while True:
+        a1, a2 = rps_input(rps_list)
+        if a1 in rps_list:
+            result = rps(a1, a2)
+            print(a2)
+            print(result)
+        else:
+            print("Invalid Input")
+            continue
+        if result == 'You Won!':
+            y_score += 1
+        elif result == 'You Lost!':
+            sys_score += 1
+        print("Your Score:{}".format(y_score))
+        print("Opponent Score:{}".format(sys_score))
+        while True:
+            again = input("Play again (y/n): ").lower()
+            if again in ("y", "n"):
+                break
+            print("Please enter y or n")
+
+        if again == "n":
+            print("Thanks for Playing 😊")
+            break
+
+
+if __name__=='__main__':
+    main()
